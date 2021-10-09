71d095ec3461f223396bd46d5635849c404b9292
Khorunzhiy Maksim <67907690+mkhorunzhiy@users.noreply.github.com>
2021-10-09 17:40:20 +0300

    Change filling and printing functions

--- main.c
+++ main.c
@@ -4,61 +4,59 @@
 #include <math.h>
 #include "array_list.h"
 
-void fillList(ArrayList* list, int length);
-void printList(ArrayList *list);
+void fillingList(ArrayList* list, int length);
+void printingList(ArrayList *list);
 
 int main() {
 
     srand(time(NULL));
-
     ArrayList* list = newArrayList();
 
-    printf("Using function add 6 times\n");
+    printf("After using function add 6 times:\n");
     fillList(list, 6);
     printList(list);
     printf("\n");
 
-    printf("Using function insert 3 times\n");
-    insert(list, 2, 101);
-    insert(list, 3, 102);
-    insert(list, 4, 103);
-    printList(list);
-    printf("\n");
 
-    printf("Using function remove 2 times\n");
+    printf("After using function remove 2 times:\n");
     removeElement(list, list->length - 1);
     removeElement(list, list->length - 1);
     printList(list);
     printf("\n");
 
-    printf("Using function set 2 times\n");
+    printf("After using function insert 3 times:\n");
+    insert(list, 2, 11);
+    insert(list, 3, 22);
+    insert(list, 4, 12);
+    printList(list);
+    printf("\n");
+
+    printf("After using function set 2 times:\n");
     set(list, 0, 3);
     set(list, 2, 21);
     printList(list);
     printf("\n");
 
-    printf("Using function get 2 times\n");
-    printf("Element by index 0 is %d\n", get(list, 0));
-    printf("Element by index 3 is %d\n", get(list, 3));
+    printf("After using function get 2 times:\n");
+    printf("array[0] : %d\n", get(list, 0));
+    printf("array[3] : %d\n", get(list, 3));
     printf("\n");
 
-    printf("Using function size\n");
-    printf("Size is %d\n\n", size(list));
+    printf("After function size:\n");
+    printf("Size :  %d\n", size(list));
 
     return 0;
 }
-
-void fillList(ArrayList* list, int length) {
+void fillingList(ArrayList* list, int length) {
     int index;
     for (index = 0; index < length; index++) {
-        add(list, rand() % 10);
+        add(list, rand() % 100 + 1);
     }
 }
-
-void printList(ArrayList* list) {
+void printingList(ArrayList* list) {
     int index;
     for (index = 0; index < list->length; index++) {
-        printf("%d ", get(list, index));
+        printf("%d, ", get(list, index));
     }
     printf("\n");
 }
